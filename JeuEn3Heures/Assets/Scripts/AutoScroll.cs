5a301c127d1bad280dae1f9bc8965aa27c74cada
Galaan <galaan.rouzier@e-artsup.net>
2023-01-30 10:08:53 +0100

    AutoScroll Finis !

--- JeuEn3Heures/Assets/Scripts/AutoScroll.cs
+++ JeuEn3Heures/Assets/Scripts/AutoScroll.cs
@@ -5,9 +5,31 @@ using UnityEngine;
 public class AutoScroll : MonoBehaviour
 {
     public float scrollSpeed = 10.0f;
+    public Transform player;
+
+    private Camera mainCamera;
+    private bool playerIsDead = false;
+
+    void Start()
+    {
+        mainCamera = Camera.main;
+    }
 
     void Update()
     {
+        if (playerIsDead)
+        {
+            return;
+        }
+
         transform.position += Vector3.right * scrollSpeed * Time.deltaTime;
+
+        Vector3 viewportPoint = mainCamera.WorldToViewportPoint(player.position);
+        if (viewportPoint.x < 0 || viewportPoint.x > 1)
+        {
+            // player has gone off screen and should die
+            playerIsDead = true;
+            Destroy(player.gameObject);
+        }
     }
 }
